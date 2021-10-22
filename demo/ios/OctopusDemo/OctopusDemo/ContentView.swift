82fcd5b3464364805b7a146c9504cec416abba5d
Ian Lavery <ian@picovoice.ai>
2021-10-22 16:20:17 -0700

    demo updates

--- demo/ios/OctopusDemo/OctopusDemo/ContentView.swift
+++ demo/ios/OctopusDemo/OctopusDemo/ContentView.swift
@@ -14,69 +14,144 @@ struct ContentView: View {
     let activeBlue = Color(red: 55/255, green: 125/255, blue: 1, opacity: 1)
     let detectionBlue = Color(red: 0, green: 229/255, blue: 195/255, opacity: 1)
     let dangerRed = Color(red: 1, green: 14/255, blue: 14/255, opacity: 1)
-
+    
     var body: some View {
-        let isError = viewModel.errorMessage.count > 0
-        let btnColor = (isError) ? Color.gray : activeBlue
-        let errorMsgColor = (isError) ? dangerRed : Color.white
-
-        Spacer();
-        Spacer();        
-
+        let interactDisabled: Bool = viewModel.state == UIState.FATAL_ERROR || viewModel.isBusy
+        
         VStack(alignment: .center){
-            Button(action: viewModel.toggleRecording){
-                Text(viewModel.recordToggleButtonText)
-                    .font(.title)
-                    .background(btnColor)
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 35.0)
-                    .padding(.vertical, 20.0)
-            }.background(
-                Capsule().fill(btnColor)
-            ).padding(12).disabled(isError);
-            Spacer();
+            Spacer()
+            ZStack(alignment: .center) {
+                Text(String(format: "%.1f", viewModel.recordingTimeSec))
+                    .font(.system(size: 60.0))
+                    .padding(20)
+                    .opacity(viewModel.state == UIState.RECORDING ? 1 : 0)
+                ProgressView("Indexing...")
+                    .progressViewStyle(CircularProgressViewStyle(tint: activeBlue))
+                    .scaleEffect(x:2, y:2, anchor: .center)
+                    .opacity(viewModel.state == UIState.INDEXING ? 1 : 0)
+                    .padding(20)
+                	
+                VStack(alignment: .center) {
+                    Text(viewModel.searchResultCountText)
+                        .font(.system(size: 25.0))
+                        .opacity(viewModel.state == UIState.SEARCH_RESULTS ||
+                                    viewModel.state == UIState.ZERO_SEARCH_RESULTS ? 1 : 0)
+                        .frame(alignment: .center)
+                        .padding(.top, 30)
+                    VStack(alignment: .center) {
+                        HStack(alignment: .center) {
+                            Text("Start (sec)")
+                                .padding(3)
+                                .font(.headline)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            Text("End (sec)")
+                                .padding(3)
+                                .font(.headline)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                            Text("Probability")
+                                .padding(3)
+                                .font(.headline)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        .frame(maxWidth: .infinity)
+                        ScrollView {
+                            VStack(alignment: .center) {
+                                ForEach(viewModel.results, id: \.startSec) { result in
+                                    HStack(alignment: .center) {
+                                        Text(String(format: "%.1fs", result.startSec))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                            .foregroundColor(.white)
+                                            .padding(6)
+                                        Text(String(format: "%.1fs", result.endSec))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                            .foregroundColor(.white)
+                                            .padding(6)
+                                        Text(String(format: "%.0f%%", result.probability * 100))
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                            .foregroundColor(.white)
+                                            .padding(6)
+                                    }.background(Color(red: 0, green: 229/255, blue: 195/255, opacity: 0.1))
+                                    .frame(maxWidth: .infinity)
+                                }
+                            }
+                            .padding(10)
+                            .background(Color(red: 37/255, green: 24/255, blue: 126/255, opacity: 1))
+                            .frame(maxWidth: .infinity)
+                        }
+                    }
+                    .opacity(viewModel.state == UIState.SEARCH_RESULTS ? 1 : 0)
+                    .padding(.vertical, 30)
+                    .frame(maxWidth: .infinity)
+                }.padding(.vertical, 20)
+                .frame(maxWidth: .infinity)
+            }.frame(maxWidth: .infinity)
 
-            Text(viewModel.messageText);
-            Spacer();
+            Spacer()
+            Spacer()
+            Spacer()
             
             HStack(alignment: .center) {
-                TextField("Search Phrase", text: $viewModel.searchPhraseText)
+                TextField("Search Phrase",
+                          text: $viewModel.searchPhraseText,
+                          onCommit: viewModel.searchMetadata)
                     .padding(7)
                     .padding(.horizontal, 25)
                     .background(Color(.systemGray6))
                     .cornerRadius(8)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                     .overlay(HStack() {
                         Image(systemName: "magnifyingglass")
                             .foregroundColor(.gray)
                             .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                             .padding(.leading, 8)
                     })
-                    .disabled(isError)
+                    .disabled(interactDisabled)
                 Button(action: viewModel.searchMetadata){
                     Text("Search")
-                }.disabled(isError)
-            }.padding(.horizontal, 48);
-            List {
-                ForEach(viewModel.results, id: \.startSec) { result in
-                    HStack() {
-                        Text(String(format: "Start: %.1fs", result.startSec));
-                        Text(String(format: "End: %.1fs", result.endSec));
-                        Text(String(format: "Probability: %.0f%%", result.probability * 100));
-                    }
-                }
+                }.disabled(interactDisabled)
             }
-            Spacer()
-
+            .padding(.horizontal, 10)
+            .opacity(viewModel.state == UIState.NEW_SEARCH ||
+                     viewModel.state == UIState.SEARCH_RESULTS ||
+                     viewModel.state == UIState.ZERO_SEARCH_RESULTS ? 1 : 0)
+            
             Text(viewModel.errorMessage)
                 .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width - 50)
-                .padding(.vertical, 10)
-                .padding(.horizontal, 10)
+                .padding()
                 .font(.body)
-                .background(errorMsgColor)
+                .background(dangerRed)
                 .foregroundColor(.white)
                 .cornerRadius(.infinity)
+                .opacity(viewModel.state == UIState.FATAL_ERROR ? 1 : 0)
+                
             Spacer()
-        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            Text(viewModel.statusText)
+                .padding(4)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+            Button(action: viewModel.toggleRecording){
+                Text(viewModel.state == UIState.RECORDING ? "STOP" : "RECORD")
+                    .font(.title)
+                    .background(interactDisabled ? Color.gray : activeBlue)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 35.0)
+                    .padding(.vertical, 20.0)
+            }.background(
+                Capsule().fill(interactDisabled ? Color.gray : activeBlue)
+            )
+            .padding(12)
+            .disabled(interactDisabled)
+        }
+        .padding(25)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .alert(isPresented: $viewModel.showErrorAlert,
+               content: {
+                Alert(
+                    title: Text("Error").foregroundColor(Color.red),
+                    message: Text(viewModel.errorAlertMessage),
+                    dismissButton: .default(Text("OK")))
+               })
     }
 }
 
--- demo/ios/OctopusDemo/OctopusDemo/ViewModel.swift
+++ demo/ios/OctopusDemo/OctopusDemo/ViewModel.swift
@@ -11,6 +11,16 @@ import AVFoundation
 import Foundation
 import Octopus
 
+enum UIState {
+    case INTRO
+    case RECORDING
+    case INDEXING
+    case NEW_SEARCH
+    case ZERO_SEARCH_RESULTS
+    case SEARCH_RESULTS
+    case FATAL_ERROR
+}
+
 class ViewModel: ObservableObject {
     
     private let ACCESS_KEY = "{YOUR_ACCESS_KEY_HERE}"
@@ -18,87 +28,148 @@ class ViewModel: ObservableObject {
     private var octopus:Octopus!
     private var metadata:OctopusMetadata!
     
+    private var recordingTimer = Timer()
     private var audioRecorder: AVAudioRecorder!
     private var isListening = false
+    private let MAX_RECORDING_LENGTH_SEC = 120.0
     
     @Published var recordToggleButtonText:String = "Start"
     @Published var searchPhraseText:String = ""
     @Published var results:[OctopusMatch] = []
-    @Published var messageText = "Press Start to begin recording audio"
+    @Published var statusText = ""
+    @Published var showErrorAlert = false
+    @Published var errorAlertMessage = ""
     @Published var errorMessage = ""
+    @Published var state:UIState = UIState.INTRO
+    @Published var isBusy:Bool = false
+    @Published var searchResultCountText:String = "# matches found"
+    @Published var recordingTimeSec = 0.0
     
     init() {
+        isBusy = true
         do {
             try octopus = Octopus(accessKey: ACCESS_KEY)
+            statusText = "Start by recording some audio"
+            isBusy = false
         } catch OctopusError.OctopusInvalidArgumentError {
-            errorMessage = "ACCESS_KEY '\(ACCESS_KEY)' is invalid."
+            onOctopusInitFail("ACCESS_KEY '\(ACCESS_KEY)' is invalid")
         } catch OctopusError.OctopusActivationError {
-            errorMessage = "ACCESS_KEY activation error."
+            errorMessage = "ACCESS_KEY activation error"
         } catch OctopusError.OctopusActivationRefusedError {
-            errorMessage = "ACCESS_KEY activation refused."
+            errorMessage = "ACCESS_KEY activation refused"
         } catch OctopusError.OctopusActivationLimitError {
-            errorMessage = "ACCESS_KEY reached its limit."
+            errorMessage = "ACCESS_KEY reached its limit"
         } catch OctopusError.OctopusActivationThrottledError  {
-            errorMessage = "ACCESS_KEY is throttled."
+            errorMessage = "ACCESS_KEY is throttled"
         } catch {
             errorMessage = "\(error)"
         }
     }
     
     deinit {
-        try! stop();
-        octopus.delete();
+        do {
+            try stop()
+        } catch {
+            showErrorAlert("\(error)")
+        }
+        octopus.delete()
     }
     
-    public func toggleRecording(){
-        if (octopus == nil) {
-            messageText = "Octopus was not initalized"
-            return;
+    func onOctopusInitFail(_ initError:String) {
+        errorMessage = initError
+        state = UIState.FATAL_ERROR
+    }
+    
+    public func toggleRecording() {
+        if isListening {
+            toggleRecordingOff()
+        } else {
+            toggleRecordingOn()
+        }
+    }
+    
+    public func toggleRecordingOff() {
+        recordingTimer.invalidate()
+        statusText = ""
+        state = UIState.INDEXING
+        isBusy = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            do {
+                try self.stop()
+                self.results = []
+                self.searchPhraseText = ""
+                self.state = UIState.NEW_SEARCH
+                self.statusText = "Try searching for a phrase in your recording"
+            } catch {
+                self.showErrorAlert("\(error)")
+            }
+            self.isBusy = false
+        }
+    }
+    
+    public func toggleRecordingOn(){
+        isBusy = true
+        recordingTimeSec = 0
+        recordingTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            self.recordingTimeSec += 0.1
+            if(self.recordingTimeSec > self.MAX_RECORDING_LENGTH_SEC) {
+                self.toggleRecordingOff()
+                self.showErrorAlert("Recording exceeded max recording length")
+            }
         }
         
-        do {
-            if isListening {
-                messageText = "Indexing... Please wait"
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
-                    try! self.stop();
-                    self.recordToggleButtonText = "Start"
-                    self.messageText = "Audio indexing complete!"
-                }
-            } else {
-                try start();
-                results = []
-                messageText = "Recording..."
-                recordToggleButtonText = "Stop"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            do {
+                try self.start()
+                self.statusText = "Recording..."
+                self.state = UIState.RECORDING
+            } catch {
+                self.showErrorAlert("\(error)")
             }
-        } catch {
-            print("\(error)")
+            self.isBusy = false
         }
     }
     
     public func searchMetadata(){
-        guard metadata != nil else {
-            messageText = "Please record some audio first"
-            return
-        }
+        
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
         
         searchPhraseText = searchPhraseText.trimmingCharacters(in: .whitespacesAndNewlines)
         guard !searchPhraseText.isEmpty else {
-            messageText = "Please enter a search phrase"
+            showErrorAlert("Search phrase cannot be empty")
             return
         }
         
         do {
             let searchResults = try octopus.search(metadata: metadata, phrases: Set([searchPhraseText]))
             if (searchResults[searchPhraseText] != nil) {
-                results = searchResults[searchPhraseText]!;
+                results = searchResults[searchPhraseText]!
                 for result in results {
                     print(result)
                 }
-                messageText = String(format: "Found %d matches", results.count);
+                
+                statusText = ""
+                if results.count == 0 {
+                    searchResultCountText = "No matches found"
+                    state = UIState.ZERO_SEARCH_RESULTS
+                } else {
+                    let pluralMatch = results.count == 1 ? "match" : "matches"
+                    searchResultCountText = "\(results.count) \(pluralMatch) found"
+                    state = UIState.SEARCH_RESULTS
+                 }
             }
+        } catch OctopusError.OctopusInvalidArgumentError(let errorMessage) {
+            showErrorAlert(errorMessage)
         } catch {
-            print("\(error)")
+            showErrorAlert("\(error)")
         }
+    }
+    
+    public func showErrorAlert(_ message: String) {
+        errorAlertMessage = message
+        showErrorAlert = true
     }
     
     public func start() throws {
@@ -108,11 +179,13 @@ class ViewModel: ObservableObject {
         
         let audioSession = AVAudioSession.sharedInstance()
         if audioSession.recordPermission == .denied {
-            messageText = "Recording permission is required for this demo";
-            return;
+            errorMessage = "Recording permission is required for this demo"
+            state = UIState.FATAL_ERROR
+            statusText = ""
+            return
         }
         
-        try audioSession.setActive(true);
+        try audioSession.setActive(true)
         try audioSession.setCategory(AVAudioSession.Category.playAndRecord,
                                      options: [.mixWithOthers, .defaultToSpeaker, .allowBluetooth])
         
@@ -129,7 +202,7 @@ class ViewModel: ObservableObject {
             mChannelsPerFrame: 1,
             mBitsPerChannel: 16,
             mReserved: 0)
-        let format = AVAudioFormat(streamDescription: &formatDescription)!;
+        let format = AVAudioFormat(streamDescription: &formatDescription)!
         
         audioRecorder = try AVAudioRecorder(url: audioFilename, format: format)
         audioRecorder.record()
@@ -148,7 +221,7 @@ class ViewModel: ObservableObject {
         let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let directoryContents = try fileManager.contentsOfDirectory(at: documentDirectory, includingPropertiesForKeys: nil)
         
-        let path = directoryContents[0].path;
-        metadata = try octopus.indexAudioFile(path: path);
+        let path = directoryContents[0].path
+        metadata = try octopus.indexAudioFile(path: path)
     }
 }

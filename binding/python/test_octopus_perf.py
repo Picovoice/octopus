09884f5a04287568e54ce53c8ad7388ceee9876c
Alireza Kenarsari <alireza@picovoice.ai>
2022-08-04 06:58:59 -0700

    fix

--- binding/python/test_octopus_perf.py
+++ binding/python/test_octopus_perf.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 #
 
+import os
 import sys
 import time
 import unittest
@@ -22,20 +23,18 @@ from util import *
 
 
 class OctopusTestCase(unittest.TestCase):
-    ACCESS_KEY = sys.argv[1]
-    NUM_TEST_ITERATIONS = int(sys.argv[2])
-    INDEX_PERFORMANCE_THRESHOLD_SEC = float(sys.argv[3])
-    SEARCH_PERFORMANCE_THRESHOLD_SEC = float(sys.argv[3])
-
     octopus = None
 
     @classmethod
     def setUpClass(cls):
         access_key = sys.argv[1]
+        cls.num_test_iterations = int(sys.argv[2])
+        cls.index_performance_threshold_sec = float(sys.argv[3])
+        cls.search_performance_threshold_sec = float(sys.argv[4])
         cls.octopus = Octopus(
             access_key=access_key,
             library_path=pv_library_path('../..'),
-            model_path=pv_model_path('../..'))
+            model_path=pv_model_path('../..', 'en'))
 
         cls.audio = read_wav_file(
             os.path.join(os.path.dirname(__file__), '../../res/audio/multiple_keywords.wav'),
@@ -48,7 +47,7 @@ class OctopusTestCase(unittest.TestCase):
 
     def test_index(self):
         perf_results = []
-        for i in range(self.NUM_TEST_ITERATIONS):
+        for i in range(self.num_test_iterations):
             start = time.time()
             self.octopus.index_audio_data(self.audio)
             index_time = time.time() - start
@@ -56,15 +55,15 @@ class OctopusTestCase(unittest.TestCase):
             if i > 0:
                 perf_results.append(index_time)
 
-        avg_perf = sum(perf_results) / self.NUM_TEST_ITERATIONS
+        avg_perf = sum(perf_results) / self.num_test_iterations
         print("Average index performance: %s" % avg_perf)
-        self.assertLess(avg_perf, self.INDEX_PERFORMANCE_THRESHOLD_SEC)
+        self.assertLess(avg_perf, self.index_performance_threshold_sec)
 
     def test_search(self):
         metadata = self.octopus.index_audio_data(self.audio)
 
         perf_results = []
-        for i in range(self.NUM_TEST_ITERATIONS):
+        for i in range(self.num_test_iterations):
             start = time.time()
             self.octopus.search(metadata, ['avocado'])
             index_time = time.time() - start
@@ -72,12 +71,13 @@ class OctopusTestCase(unittest.TestCase):
             if i > 0:
                 perf_results.append(index_time)
 
-        avg_perf = sum(perf_results) / self.NUM_TEST_ITERATIONS
+        avg_perf = sum(perf_results) / self.num_test_iterations
         print("Average search performance: %s" % avg_perf)
-        self.assertLess(avg_perf, self.INDEX_PERFORMANCE_THRESHOLD_SEC)
+        self.assertLess(avg_perf, self.search_performance_threshold_sec)
 
 
 if __name__ == '__main__':
+    print(sys.argv)
     if len(sys.argv) != 5:
         print("usage: test_octopus_perf.py ${ACCESS_KEY} ${NUM_TEST_INTERVALS} "
               "${INDEX_PERFORMANCE_THRESHOLD_SEC} ${SEARCH_PERFORMANCE_THRESHOLD_SEC}")
